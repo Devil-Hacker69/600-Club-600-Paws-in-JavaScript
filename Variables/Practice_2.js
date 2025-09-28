e21290ca26dc249e84ded2bdc1491140d319531d
Devil_Hacker <118067535+Devil-Hacker69@users.noreply.github.com>
2025-09-28 21:58:35 +0600

    Add files via upload

---
+++ Variables/Practice_2.js
@@ -0,0 +1,28 @@
+const age = 22;
+console.log(age);
+
+const booksToRead = 3;
+console.log(booksToRead);
+
+const wakeUpTime = 5;
+console.log(wakeUpTime);
+
+const wastedTime = 2;
+console.log(wastedTime);
+
+const borrow = 0;
+console.log(borrow);
+
+const goals = 5;
+console.log(goals);
+
+const pillow = 2;
+console.log(pillow);
+
+
+// Here const means declare the variable 
+// and here the variable is (age, number_of_siblings, students, marks, rooms, daily_study_hours and total) 
+// also after the equal (=) The numbers is the value of these variables.
+// console means for display or output 
+// log means to show the value of the variable or text in the bracket ()
+

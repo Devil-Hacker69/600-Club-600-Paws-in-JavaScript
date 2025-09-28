2027f65e777cd45d4273f72776f86e2a49175ea6
Devil_Hacker <118067535+Devil-Hacker69@users.noreply.github.com>
2025-09-28 21:38:14 +0600

    Add files via upload

--- Variables/Practice_1.js
+++ Variables/Practice_1.js
@@ -7,3 +7,8 @@ const rooms = 5;
 const daily_study_hours = 4;
 const total = 44;
 
+// Here const means declare the variable 
+// and here the variable is (age, number_of_siblings, students, marks, rooms, daily_study_hours and total) 
+// also after the equal (=) The numbers is the value of these variables.
+
+

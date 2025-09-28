080f0c87b8813ec1f14a72f834b664a8ef6d442b
Devil_Hacker <118067535+Devil-Hacker69@users.noreply.github.com>
2025-09-28 21:53:32 +0600

    Add files via upload

---
+++ Variables/Practice_2.js
@@ -0,0 +1,22 @@
+const age = 22;
+console.log(age);
+
+const booksToRead = 3;
+console.log(booksToRead);
+
+const wakeUpTime = 5;
+console.log(wakeUpTime);
+
+const wastedTime = 2;
+console.log(wastedTime);
+
+const borrow = 0;
+console.log(borrow);
+
+const goals = 5;
+console.log(goals);
+
+const pillow = 2;
+console.log(pillow);
+
+

a4ef3bcabd806092a047c91d3c2d52657fb0d556
Devil_Hacker <118067535+Devil-Hacker69@users.noreply.github.com>
2025-10-01 15:50:20 +0600

    Add files via upload

---
+++ Variables/Practice_4.js
@@ -0,0 +1,23 @@
+const isHeHappy = false;
+console.log(isHeHappy);
+
+const receivedMessage = false;
+console.log(receivedMessage);
+
+const celebrating = true;
+console.log(celebrating);
+
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+console.log(isHappy, hasMoney, isHungry);
+
+const isStudyDone = false;
+console.log(isStudyDone);
+
+const isPhoneCharged = false;
+console.log(isPhoneCharged);
+
+const isDinnerDone = true;
+console.log(isDinnerDone);
+

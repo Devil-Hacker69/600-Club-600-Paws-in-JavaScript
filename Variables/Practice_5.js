f5b259d9c0353907e757b95c6d0b4a7059c4ebc0
Devil_Hacker <118067535+Devil-Hacker69@users.noreply.github.com>
2025-10-01 17:29:29 +0600

    Add files via upload

---
+++ Variables/Practice_5.js
@@ -0,0 +1,55 @@
+const earnedMoney = 100;
+const expense = 40;
+const leftOver_money = earnedMoney - expense;
+console.log(leftOver_money);
+
+const total_Pencil = 10;
+const Pencil_per_use = 2;
+const number_of_uses = total_Pencil / Pencil_per_use;
+console.log(number_of_uses);
+
+
+const total_Money = 60;
+const expense_per_time = 15;
+const number_of_expenses = total_Money / expense_per_time;
+const leftover_money = total_Money % expense_per_time;
+console.log(number_of_expenses, leftover_money);
+
+const string1 = "Hello";
+const string2 = "World";
+const combinedString = string1 + string2;
+console.log(combinedString);
+
+const riceWeight = 153;
+const lentilWeight = 261;
+const totalWeight = riceWeight + lentilWeight;
+console.log(totalWeight);
+
+const Total_Money = 500;
+const Expense_Per_Time = 75;
+const Number_Of_Expenses = Math.floor(Total_Money / Expense_Per_Time);
+const LeftOver_Money = Total_Money % Expense_Per_Time;
+console.log(Number_Of_Expenses, LeftOver_Money);
+
+const dividend = 8;
+const divisor = 3;
+const quotient = Math.floor(dividend / divisor);
+const remainder = dividend % divisor;
+console.log(quotient, remainder);
+
+
+const Dividend = 50;
+const Divisor = 9;
+const Remainder = Dividend % Divisor;
+console.log(Remainder);
+
+const String1 = "Bangla";
+const String2 = "desh";
+console.log(String1 + String2);
+
+const Total_rice = 480;
+const each_day_need_of_rice = 4;
+const number_of_days = Total_rice / each_day_need_of_rice;
+const number_of_months = number_of_days / 30;
+console.log(number_of_days, number_of_months);
+

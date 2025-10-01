e86e72f22e1290b0585a00647459efb89b14f524
Devil_Hacker <118067535+Devil-Hacker69@users.noreply.github.com>
2025-10-01 15:21:16 +0600

    Add files via upload

---
+++ Variables/Practice_3.js
@@ -0,0 +1,21 @@
+const name = "Md. Robiul Islam";
+console.log(name);
+
+const address = "Holding No- E-1/3, ward no. 08, Purbo chandora(Passa gate), Shafipur, kaliakoir, Gazipur.";
+console.log(address);
+
+const bestfriendInstitute = "Uttara University";
+console.log(bestfriendInstitute);
+
+const fan = "Katrina Kaif";
+console.log(fan);
+
+const movieOfTheYear = "One Battle After Another";
+console.log(movieOfTheYear);
+
+const hobby = "Bike Riding";
+console.log(hobby);
+
+const color = "Black";
+console.log(color);
+
